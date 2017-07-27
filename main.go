f4fe123d24fdb72572ad039d6fc99cea405f8e40
Tamara Kaufler <tkaufler@gmail.com>
2017-07-27 12:35:17 +0100

    Changes layout and content; implements emailing

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"net/mail"
+	"net/smtp"
+	"os"
 	"path/filepath"
+	"strings"
 	"sync"
 )
+
+var emailHost string = os.Getenv("SMTP_host")
+var emailPort string = os.Getenv("SMTP_port")
+var emailPass string = os.Getenv("SMTP_pass")
+var emailFrom string = "noreply@guitar-lessons-stalbans.co.uk"
+
+var emailRecipient string = "xxxxx@gmail.com"
 
 type templateHandler struct {
 	once     sync.Once
@@ -15,30 +27,157 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
+type IntroData struct {
+	Message string
+	Error   bool
+}
+
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		fmt.Println("Parsing template once ...")
-
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 
-	fmt.Println("Executing template ...")
 	t.templ.Execute(w, nil)
 }
 
 func main() {
+
+	if emailPass == "" {
+		log.Fatal("ERROR: email authentication (password) must be supplied for SMTP BTInternet server and xxxxx@btinternet.com")
+	}
+	if emailHost == "" {
+		emailHost = "mail.btinternet.com"
+	}
+	if emailPort == "" {
+		emailPort = "465"
+	}
+	emailServer := strings.Join([]string{emailHost, emailPort}, ":")
+
+	// static files
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// show the main page
 	http.Handle("/", &templateHandler{filename: "intro.html"})
+
 	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
 
-		// gather form data
-		//
+		t := templateHandler{filename: "intro.html"}
+
+		t.once.Do(func() {
+			t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		})
+		err := r.ParseForm()
+
+		if err != nil {
+			log.Println(err)
+
+			error := IntroData{err.Error(), true}
+			t.templ.Execute(w, error)
+
+			return
+		}
+
+		email := r.PostFormValue("email")
+
+		if email == "" {
+			t.templ.Execute(w, nil)
+			return
+		}
+
+		firstName := r.PostFormValue("first_name")
+		lastName := r.PostFormValue("last_name")
+		note := r.PostFormValue("note")
+
+		fmt.Printf("\t\tForm data : firstName=%s, lastName=%s,email=%s,note=%s\n", firstName, lastName, email, note)
+
+		subject := fmt.Sprintf("Guitar lessons interest (%s %s)", firstName, lastName)
+
+		message := "First name: %s \r\nLast name: %s \r\n\r\nEmail: %s \r\n\r\nNote: %s\r\n"
+		emailBody := fmt.Sprintf(message, firstName, lastName, email, note)
+
+		from := mail.Address{"", emailFrom}
+		to1 := mail.Address{"", emailRecipient}
+		emailSubject := subject
+
+		// email headers
+		headers := make(map[string]string)
+		headers["From"] = from.String()
+		headers["To"] = to1.String()
+		headers["Subject"] = emailSubject
+
+		// Setup message
+		emailMessage := ""
+		for k, v := range headers {
+			emailMessage += fmt.Sprintf("%s: %s\r\n", k, v)
+		}
+		emailMessage += "\r\n" + emailBody
+
+		// store in database
+
+		// send an email about the contact
+		auth := smtp.PlainAuth("", "xxxxx@btinternet.com", emailPass, "mail.btinternet.com")
+
+		// TLS config
+		tlsconfig := &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         emailHost,
+		}
+
+		// need to call tls.Dial instead of smtp.Dial
+		// for smtp servers running on 465 that require an ssl connection
+		// from the very beginning (no starttls) (https://gist.github.com/chrisgillis/10888032)
+		conn, err := tls.Dial("tcp", emailServer, tlsconfig)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		c, err := smtp.NewClient(conn, emailHost)
+		if err != nil {
+			log.Panic(err)
+		}
+
+		// Auth
+		if err = c.Auth(auth); err != nil {
+			log.Panic(err)
+		}
+
+		// To && From
+		if err = c.Mail(from.Address); err != nil {
+			log.Panic(err)
+		}
+
+		if err = c.Rcpt(to1.Address); err != nil {
+			log.Panic(err)
+		}
+
+		// Data
+		ew, err := c.Data()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		_, err = ew.Write([]byte(emailMessage))
+		if err != nil {
+			log.Panic(err)
+		}
+
+		err = ew.Close()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		c.Quit()
+
+		// show the successful message
+		successMsg := IntroData{"Thank you. Form was successfully submitted. I shall contact you shortly.", false}
+
+		t.templ.Execute(w, successMsg)
 	})
 
+	// ------- WEB SERVER -------
 	fmt.Println("Starting web server ...")
-	if err := http.ListenAndServe(":3030", nil); err != nil {
+	if err := http.ListenAndServe("192.168.1.91:80", nil); err != nil {
 		log.Fatal("Server problems: ", err)
 	}
 }
